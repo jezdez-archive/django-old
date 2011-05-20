d49fa052e365fc447cea9b4affe4ea7ebc3cdebe
Jannis Leidel <jannis@leidel.info>
2011-05-20 23:28:21 +0200

    Pass over baseconv to be able to handle more bases.

--- django/utils/baseconv.py
+++ django/utils/baseconv.py
@@ -1,31 +1,70 @@
+# Copyright (c) 2010 Taurinus Collective. All rights reserved.
+# Copyright (c) 2009 Simon Willison. All rights reserved.
+# Copyright (c) 2002 Drew Perttula. All rights reserved.
+#
+# License:
+#   Python Software Foundation License version 2
+#
+# See the file "LICENSE" for terms & conditions for usage, and a DISCLAIMER OF
+# ALL WARRANTIES.
+#
+# This Baseconv distribution contains no GNU General Public Licensed (GPLed)
+# code so it may be used in proprietary projects just like prior ``baseconv``
+# distributions.
+#
+# All trademarks referenced herein are property of their respective holders.
+#
+
 """
 Convert numbers from base 10 integers to base X strings and back again.
 
-Sample usage:
+Sample usage::
 
->>> base20 = BaseConverter('0123456789abcdefghij')
->>> base20.encode(1234)
-'31e'
->>> base20.decode('31e')
-1234
+  >>> base20 = BaseConverter('0123456789abcdefghij')
+  >>> base20.encode(1234)
+  '31e'
+  >>> base20.decode('31e')
+  1234
+  >>> base20.encode(-1234)
+  '-31e'
+  >>> base20.decode('-31e')
+  -1234
+  >>> base11 = BaseConverter('0123456789-', sign='$')
+  >>> base11.encode('$1234')
+  '$-22'
+  >>> base11.decode('$-22')
+  '$1234'
+
 """
 
+BASE2_ALPHABET = '01'
+BASE16_ALPHABET = '0123456789ABCDEF'
+BASE56_ALPHABET = '23456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnpqrstuvwxyz'
+BASE36_ALPHABET = '0123456789abcdefghijklmnopqrstuvwxyz'
+BASE62_ALPHABET = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+BASE64_ALPHABET = BASE62_ALPHABET + '-_'
 
 class BaseConverter(object):
-    decimal_digits = "0123456789"
+    decimal_digits = '0123456789'
 
-    def __init__(self, digits):
+    def __init__(self, digits, sign='-'):
+        self.sign = sign
         self.digits = digits
+
+    def __repr__(self):
+        return "<BaseConverter: base%s (%s)>" % (len(self.digits), self.digits)
 
     def encode(self, i):
         return self.convert(i, self.decimal_digits, self.digits)
 
     def decode(self, s):
-        return int(self.convert(s, self.digits, self.decimal_digits))
+        value = self.convert(s, self.digits, self.decimal_digits)
+        if self.sign == '-':
+            return int(value)
+        return value
 
-    def convert(number, fromdigits, todigits):
-        # Based on http://code.activestate.com/recipes/111286/
-        if str(number)[0] == '-':
+    def convert(self, number, from_digits, to_digits):
+        if str(number)[0] == self.sign:
             number = str(number)[1:]
             neg = 1
         else:
@@ -34,25 +73,24 @@ class BaseConverter(object):
         # make an integer out of the number
         x = 0
         for digit in str(number):
-            x = x * len(fromdigits) + fromdigits.index(digit)
+            x = x * len(from_digits) + from_digits.index(digit)
 
-        # create the result in base 'len(todigits)'
+        # create the result in base 'len(to_digits)'
         if x == 0:
-            res = todigits[0]
+            res = to_digits[0]
         else:
-            res = ""
+            res = ''
             while x > 0:
-                digit = x % len(todigits)
-                res = todigits[digit] + res
-                x = int(x / len(todigits))
+                digit = x % len(to_digits)
+                res = to_digits[digit] + res
+                x = int(x / len(to_digits))
             if neg:
-                res = '-' + res
+                res = self.sign + res
         return res
-    convert = staticmethod(convert)
 
-base2 = BaseConverter('01')
-base16 = BaseConverter('0123456789ABCDEF')
-base36 = BaseConverter('0123456789abcdefghijklmnopqrstuvwxyz')
-base62 = BaseConverter(
-    '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-)
+base2 = BaseConverter(BASE2_ALPHABET)
+base16 = BaseConverter(BASE16_ALPHABET)
+base36 = BaseConverter(BASE36_ALPHABET)
+base56 = BaseConverter(BASE56_ALPHABET)
+base62 = BaseConverter(BASE62_ALPHABET)
+base64 = BaseConverter(BASE64_ALPHABET, sign='$')
--- tests/regressiontests/utils/baseconv.py
+++ tests/regressiontests/utils/baseconv.py
@@ -1,11 +1,29 @@
 from unittest import TestCase
-from django.utils.baseconv import base2, base16, base36, base62
+from django.utils.baseconv import base2, base16, base36, base56, base62, base64, BaseConverter
 
 class TestBaseConv(TestCase):
 
     def test_baseconv(self):
         nums = [-10 ** 10, 10 ** 10] + range(-100, 100)
-        for convertor in [base2, base16, base36, base62]:
-            for i in nums:
-                self.assertEqual(i, convertor.decode(convertor.encode(i)))
+        for converter in [base2, base16, base36, base56, base62, base64]:
+            if converter.sign == '-':
+                for i in nums:
+                    self.assertEqual(i, converter.decode(converter.encode(i)))
+            else:
+                for i in nums:
+                    i = str(i)
+                    if i[0] == '-':
+                        i = converter.sign + i[1:]
+                    self.assertEqual(i, converter.decode(converter.encode(i)))
 
+    def test_base20(self):
+        base20 = BaseConverter('0123456789abcdefghij')
+        self.assertEqual(base20.encode(1234), '31e')
+        self.assertEqual(base20.decode('31e'), 1234)
+        self.assertEqual(base20.encode(-1234), '-31e')
+        self.assertEqual(base20.decode('-31e'), -1234)
+
+    def test_base11(self):
+        base11 = BaseConverter('0123456789-', sign='$')
+        self.assertEqual(base11.encode('$1234'), '$-22')
+        self.assertEqual(base11.decode('$-22'), '$1234')
