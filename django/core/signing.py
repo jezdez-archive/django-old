0ce9f89046728f6d91f924bb1794dbde39c37777
Jannis Leidel <jannis@leidel.info>
2011-05-20 22:07:49 +0200

    Removed uneeded imports.

--- django/core/signing.py
+++ django/core/signing.py
@@ -33,8 +33,6 @@ There are 65 url-safe characters: the 64 used by url-safe base64 and the '.'.
 These functions make use of all of them.
 """
 import base64
-import hashlib
-import random
 import time
 import zlib
 
