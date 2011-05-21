7280fb01414a659489ed8d07932bb96b91b627d1
Stephan Jaekel <steph@rdev.info>
2011-05-21 21:25:09 +0200

    Fixed docstring.

--- django/contrib/formtools/wizard/forms.py
+++ django/contrib/formtools/wizard/forms.py
@@ -2,8 +2,6 @@ from django import forms
 
 class ManagementForm(forms.Form):
     """
-    ``ManagementForm`` is used to keep track of how many form instances
-    are displayed on the page. If adding new forms via javascript, you should
-    increment the count field of this form as well.
+    ``ManagementForm`` is used to keep track of the current wizard step.
     """
     current_step = forms.CharField(widget=forms.HiddenInput)
