38404bc4004035758580d65441409d2d0ec6efdc
Gregor Müllegger <gregor@muellegger.de>
2011-06-26 13:10:14 +0200

    Tests for nested usage of assertTemplateUsed context manager.

--- tests/regressiontests/test_utils/tests.py
+++ tests/regressiontests/test_utils/tests.py
@@ -109,6 +109,21 @@ class AssertTemplateUsedContextManagerTests(TestCase):
             render_to_string('template_used/base.html')
             render_to_string('template_used/base.html')
 
+    def test_nested_usage(self):
+        with self.assertTemplateUsed('template_used/base.html'):
+            with self.assertTemplateUsed('template_used/include.html'):
+                render_to_string('template_used/include.html')
+
+        with self.assertTemplateUsed('template_used/extends.html'):
+            with self.assertTemplateUsed('template_used/base.html'):
+                render_to_string('template_used/extends.html')
+
+        with self.assertTemplateUsed('template_used/base.html'):
+            with self.assertTemplateUsed('template_used/alternative.html'):
+                render_to_string('template_used/alternative.html')
+            render_to_string('template_used/base.html')
+
+
     def test_error_message(self):
         try:
             with self.assertTemplateUsed('template_used/base.html'):
