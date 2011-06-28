75a0bc9d3756e7d218b8b206e8505f40d0a20889
Jannis Leidel <jannis@leidel.info>
2011-06-28 18:23:16 +0200

    Added deprecation warning if ADMIN_MEDIA_PREFIX is defined in settings (per Jacob).

--- django/conf/__init__.py
+++ django/conf/__init__.py
@@ -70,6 +70,9 @@ class BaseSettings(object):
         if name in ("MEDIA_URL", "STATIC_URL") and value and not value.endswith('/'):
             warnings.warn('If set, %s must end with a slash' % name,
                           DeprecationWarning)
+        elif name == "ADMIN_MEDIA_PREFIX":
+            warnings.warn("The ADMIN_MEDIA_PREFIX setting has been removed; "
+                          "use STATIC_URL instead.", DeprecationWarning)
         object.__setattr__(self, name, value)
 
 
