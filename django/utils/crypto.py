71c40d863b3e9db35010435ec687dcb7580cb68e
Justine Tunney <justine@gothbook.local>
2011-09-10 18:33:09 -0700

    make pbkdf2 use sha256

--- django/utils/crypto.py
+++ django/utils/crypto.py
@@ -51,7 +51,7 @@ class PBKDF2RandomSource(object):
     For example::
 
         import hashlib
-        prf = PBKDF2RandomSource(hashlib.sha512)
+        prf = PBKDF2RandomSource(hashlib.sha256)
 
     """
 
@@ -67,8 +67,10 @@ def pbkdf2(password, salt, iterations=2000, dklen=0, prf=None):
     """
     Implements PBKDF2 as defined in RFC 2898, section 5.2
 
-    This routine was originally written by Christian Aichinger:
+    Based on a routine written by aaz:
     http://stackoverflow.com/questions/5130513/pbkdf2-hmac-sha2-test-vectors
+
+    DO NOT change the default behavior of this function.  Ever.
 
     For example::
 
@@ -76,8 +78,9 @@ def pbkdf2(password, salt, iterations=2000, dklen=0, prf=None):
         'afe6c5530785b6cc6b1c6453384731bd5ee432ee'
 
     """
+    assert iterations > 0
     if not prf:
-        prf = PBKDF2RandomSource(hashlib.sha512)
+        prf = PBKDF2RandomSource(hashlib.sha256)
     hlen = prf.digest_size
     if not dklen:
         dklen = hlen
